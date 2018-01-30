4f73b180239c4a5cfc2117b2027eed67dcdd4f4a
truexinology <truexin@163.com>
2018-01-30 14:42:59 +0800

    [refactor] initial file

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,10 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home'
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Home'
+        }
+    ]
 })
